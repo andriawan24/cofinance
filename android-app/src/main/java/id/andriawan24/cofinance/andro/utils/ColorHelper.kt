f1611b76d43b30e5d043ca96ca87acea80d85414
andriawan24 <fawaznaufal23@gmail.com>
2025-06-24 22:39:40 +0700

    feature: Implement color getter by transaction status
    Signed-off-by: andriawan24 <fawaznaufal23@gmail.com>

--- android-app/src/main/java/id/andriawan24/cofinance/andro/utils/ColorHelper.kt
+++ android-app/src/main/java/id/andriawan24/cofinance/andro/utils/ColorHelper.kt
@@ -1,9 +1,26 @@
 package id.andriawan24.cofinance.andro.utils
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
+import id.andriawan24.cofinance.andro.R
+import id.andriawan24.cofinance.utils.enums.TransactionType
 
 object ColorHelper {
+    @Composable
+    @ReadOnlyComposable
+    fun getColorByExpenseType(transactionType: TransactionType): Color {
+        return when (transactionType) {
+            TransactionType.EXPENSE -> colorResource(R.color.red)
+            TransactionType.INCOME -> colorResource(R.color.green)
+            TransactionType.TRANSFER -> MaterialTheme.colorScheme.onBackground
+        }
+    }
+
+    @Deprecated("This method is unused and it will be removed in the future")
     fun generateColorPalette(
         size: Int,
         baseColor: Color = Color(0xFF3B82F6), // Default to a blue color
