0bfdf47befbd4d6fb5dcab54708c7832e4518cc6
andriawan24 <fawaznaufal23@gmail.com>
2025-06-28 10:53:41 +0700

    feature: Add current locale checking
    Signed-off-by: andriawan24 <fawaznaufal23@gmail.com>

--- android-app/src/main/java/id/andriawan24/cofinance/andro/utils/LocaleHelper.kt
+++ android-app/src/main/java/id/andriawan24/cofinance/andro/utils/LocaleHelper.kt
@@ -1,7 +1,15 @@
 package id.andriawan24.cofinance.andro.utils
 
+import android.content.res.Resources
+import androidx.core.os.ConfigurationCompat
 import java.util.Locale
+
 
 object LocaleHelper {
     val indonesian: Locale = Locale.forLanguageTag("id-ID")
+
+    fun getCurrentLocale(): Locale {
+        return ConfigurationCompat.getLocales(Resources.getSystem().configuration).get(0)
+            ?: Locale.getDefault()
+    }
 }
