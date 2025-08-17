12ca956041a8f20fe69352987d2a6fede7d00af8
andriawan24 <fawaznaufal23@gmail.com>
2025-08-17 07:10:29 +0700

    feature: Add account group card component
    Signed-off-by: andriawan24 <fawaznaufal23@gmail.com>

---
+++ android-app/src/main/java/id/andriawan24/cofinance/andro/ui/presentation/account/AccountGroupCard.kt
@@ -0,0 +1,111 @@
+package id.andriawan24.cofinance.andro.ui.presentation.account
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import id.andriawan24.cofinance.andro.ui.presentation.account.models.AccountByGroup
+import id.andriawan24.cofinance.andro.utils.Dimensions
+import id.andriawan24.cofinance.andro.utils.NumberHelper
+import id.andriawan24.cofinance.andro.utils.ext.dropShadow
+
+@Composable
+internal fun AccountGroupCard(
+    group: AccountByGroup,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = Dimensions.SIZE_16)
+            .dropShadow(
+                shape = MaterialTheme.shapes.large,
+                blur = Dimensions.SIZE_10,
+                color = MaterialTheme.colorScheme.primary.copy(alpha = 0.08f),
+                offsetY = Dimensions.SIZE_4
+            )
+            .background(
+                color = MaterialTheme.colorScheme.onPrimary,
+                shape = MaterialTheme.shapes.large
+            )
+            .padding(Dimensions.SIZE_16)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(space = Dimensions.SIZE_12)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = group.backgroundColor,
+                        shape = MaterialTheme.shapes.small
+                    )
+                    .padding(all = Dimensions.SIZE_12),
+            ) {
+                Image(
+                    painter = painterResource(group.imageRes),
+                    contentDescription = null
+                )
+            }
+
+            Text(
+                modifier = Modifier.weight(1f),
+                text = group.groupLabel,
+                style = MaterialTheme.typography.labelMedium
+            )
+
+            Text(
+                text = NumberHelper.formatRupiah(group.totalAmount),
+                style = MaterialTheme.typography.labelMedium
+            )
+        }
+
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = Dimensions.SIZE_16),
+            thickness = Dimensions.SIZE_1,
+            color = MaterialTheme.colorScheme.surfaceContainerLow
+        )
+
+        Column(verticalArrangement = Arrangement.spacedBy(Dimensions.SIZE_12)) {
+            group.accounts.forEach { account ->
+                Row(
+                    modifier = Modifier
+                        .background(
+                            color = group.backgroundColor,
+                            shape = MaterialTheme.shapes.medium
+                        )
+                        .padding(all = Dimensions.SIZE_12),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(space = Dimensions.SIZE_12)
+                ) {
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = account.name,
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontWeight = FontWeight.Medium
+                        )
+                    )
+
+                    Text(
+                        text = NumberHelper.formatRupiah(number = account.balance),
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontWeight = FontWeight.Medium
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
