2651924cf96beaad9079af589bb1589e54493584
andriawan24 <fawaznaufal23@gmail.com>
2025-11-19 22:06:04 +0700

    fix: Modal bottom sheet pass through status bar
    Signed-off-by: andriawan24 <fawaznaufal23@gmail.com>

--- android-app/src/main/java/id/andriawan24/cofinance/andro/ui/presentation/common/BaseBottomSheet.kt
+++ android-app/src/main/java/id/andriawan24/cofinance/andro/ui/presentation/common/BaseBottomSheet.kt
@@ -1,12 +1,16 @@
 package id.andriawan24.cofinance.andro.ui.presentation.common
 
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import id.andriawan24.cofinance.andro.utils.Dimensions
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
@@ -17,13 +21,14 @@ fun BaseBottomSheet(
     content: @Composable () -> Unit
 ) {
     ModalBottomSheet(
+        modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars),
         sheetState = state,
         containerColor = MaterialTheme.colorScheme.onPrimary,
         shape = RoundedCornerShape(
             topStart = Dimensions.SIZE_10,
             topEnd = Dimensions.SIZE_10
         ),
-        onDismissRequest = onDismissRequest,
+        onDismissRequest = onDismissRequest
     ) {
         content()
     }
