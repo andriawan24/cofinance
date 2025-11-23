f00fe52e72d0c7efbcc941b03fdd27d84140e05f
andriawan24 <fawaznaufal23@gmail.com>
2025-11-23 21:19:54 +0700

    fix: Make sure status bar always dark
    Signed-off-by: andriawan24 <fawaznaufal23@gmail.com>

--- android-app/src/main/java/id/andriawan24/cofinance/andro/MainActivity.kt
+++ android-app/src/main/java/id/andriawan24/cofinance/andro/MainActivity.kt
@@ -1,7 +1,9 @@
 package id.andriawan24.cofinance.andro
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -19,7 +21,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         setContent {
             KoinApplication(
                 application = {
