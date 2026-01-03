54ba5ffcbdcd541dba16e2a1352c3d5cb66ab96c
andriawan24 <fawaznaufal23@gmail.com>
2026-01-03 22:23:18 +0700

    feature: Add datetime libs

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -54,6 +54,7 @@ kotlin {
             implementation(libs.compose.navigation)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kotlinx.datetime)
         }
 
         commonTest.dependencies {
