1c008c50933d25ec35516c0f2d1f0f6d0dfb673c
andriawan24 <fawaznaufal23@gmail.com>
2026-01-21 23:00:09 +0700

    feature: Migrate compose runtime to new libs

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -67,7 +67,7 @@ kotlin {
         }
 
         commonMain.dependencies {
-            implementation(compose.runtime)
+            implementation(libs.compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
             implementation(compose.ui)
